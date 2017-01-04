9dcf7befa0f6b895881b90782045fa8412696fa1
TasfiqulGhani <tasfiqulghani@gmail.com>
2017-01-04 22:37:37 +0600

    Codes

---
+++ src/Bird.java
@@ -0,0 +1,31 @@
+
+public class Bird {
+	
+	
+	 
+	
+	public void askName(){
+		
+		System.out.println("Hi my name is bird");
+	}
+	
+	
+
+public void askName(String name){
+		
+		System.out.println("Hi my name is  "+name);
+	
+
+}
+
+
+ private void add(int a, int b){
+	 
+	 System.out.print(a+b);
+ }
+private void add(double a, double b){
+	 
+	 System.out.print(a+b);
+ }
+
+}
---
+++ src/main.java
@@ -0,0 +1,22 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 
+		
+		
+		int number ;
+		String name;
+		
+		
+		Bird birdone =new Bird();
+		 
+	 
+		
+
+	}
+
+}
